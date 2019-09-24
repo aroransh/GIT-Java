5d28a01224127190bb74da68daf6566d65047f69
aroransh <aroraansh1990@gmail.com>
2019-09-24 14:48:41 +0530

    Adding two numbers

---
+++ Add.java
@@ -0,0 +1,14 @@
+package com.Anshul;
+
+public class Add {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=45, b=45,c;
+
+        c=a+b;
+
+        System.out.println(c);
+
+    }
+}
