c7bdfb993bab91af5b094d9ba5cb30eb0c3515a7
aroransh <aroraansh1990@gmail.com>
2019-09-28 17:46:26 +0530

    Math method in Java

---
+++ Math_method.java
@@ -0,0 +1,23 @@
+package com.Anshul;
+
+public class Math_method {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int a=5, b=6;
+        System.out.println(Math.max(a,b)); // Will print the max numbers between two integers
+        System.out.println(Math.addExact(a,b)); // will add Two integers, (you can also try this with String if you want to write "The sum of two numbers is " + Math.addExact(a,b))
+        System.out.println(Math.abs(-4)); // will make the number always positive whether it is + or -
+        System.out.println(Math.decrementExact(a)); // reduce the number by 1
+        System.out.println(Math.random());  // will give any random number between 0 and 1
+        System.out.println(2+(4-2)*Math.random()); // will give any number between 2 and 4
+
+
+
+
+
+
+
+    }
+}
