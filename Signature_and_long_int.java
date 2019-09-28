c08837a46d9f4200edde8152000419356776a1bf
aroransh <aroraansh1990@gmail.com>
2019-09-28 17:49:57 +0530

    New methods

---
+++ Signature_and_long_int.java
@@ -0,0 +1,22 @@
+package com.Anshul;
+
+import java.util.Scanner;
+
+public class Signature_and_long_int {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String name= "Anshul Arora";
+        String Designation= "Mainframe, HCL INDIA";
+        long contact= 9952027231l;
+
+        System.out.println(name + "\n" + Designation + "\n" + contact);
+        /*    System.out.println(contact);
+            System.out.println(name.length());
+            System.out.println(name.toUpperCase());
+            System.out.println(name.toUpperCase() + " from\n " + "India");
+*/
+
+    }
+}
---
+++ concatenation_with_inverted_commas.java
@@ -0,0 +1,11 @@
+package com.Anshul;
+
+public class concatenation_with_inverted_commas {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name="Anshul";
+        System.out.println(name + " from " + "\"INDIA\"");
+
+    }
+}
